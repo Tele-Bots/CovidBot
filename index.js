753bf645ac453d86d5f2d286b946ab1da5f0c2c0
Rahul Gurung <gurungrahul2@gmail.com>
2020-04-13 22:21:08 +0530

    added HTML support

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
+const TelegramBot = require('node-telegram-bot-api')
+const request = require('request')
 require('dotenv').config()
-let options = { json: true };
+let options = { json: true }
 
-const url = 'https://api.covid19india.org/data.json';
+const url = 'https://api.covid19india.org/data.json'
 
 // replace the value below with the Telegram token
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = process.env.TELEGRAM_BOT_TOKEN
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { polling: true })
 
 // Listen for any kind of message and reply with the data
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+    const chatId = msg.chat.id
+
 
     request(url, options, (error, res, body) => {
         if (error) {
             return console.log(error)
-        };
+        }
 
         if (!error && res.statusCode == 200) {
             // For full state full name, convert the user input into lower case and then
             // convert the first letters into capital
-            let user_msg_state = msg.text.toLowerCase();
+            let user_msg_state = msg.text.toLowerCase()
             user_msg_state = user_msg_state.charAt(0).toUpperCase() + user_msg_state.slice(1)
             var splitStr = user_msg_state.toLowerCase().split(' ')
             for (var i = 0; i < splitStr.length; i++) {
@@ -32,38 +33,44 @@ bot.on('message', (msg) => {
             user_msg_state = splitStr.join(' ')
 
             // For shortcut state just capitalize the user input
-            let user_msg = msg.text.toUpperCase();
+            let user_msg = msg.text.toUpperCase()
 
             let statewise = body['statewise']
-            let stateindex;
+            let stateindex
             for (let index = 0; index < statewise.length; index++) {
                 let currentdata = statewise[index]
                 if (currentdata['state'] == user_msg_state || currentdata['statecode'] == user_msg) {
-                    stateindex = index;
-                    break;
+                    stateindex = index
+                    break
                 }
             }
-            
+
             // For first time messages
             if (msg.text == "/start") {
-                bot.sendMessage(chatId, 'Welcome to Covid19 Bot.\nSend me your state code and state name and I will provide you updated Corona stats.\nFor example: \npb for Punjab\njk for Jammu and Kashmir etc');
-                return;
+                bot.sendMessage(chatId, 'Welcome to Covid19 Bot.\nSend me your state ' +
+                    'code or state name and I will provide you with updated Corona stats.' +
+                    '\nFor example: \npb for Punjab\njk for Jammu and Kashmir etc')
+                return
             }
             if (stateindex == undefined) {
-                bot.sendMessage(chatId, 'Sorry ! I am not able to understand.\nSend me your state code and state name and I will provide you updated Corona stats.\nFor example: \npb for Punjab\njk for Jammu and Kashmir etc');
-                return;
+                bot.sendMessage(chatId,
+                    'Sorry ! I am not able to understand.\nSend me your state code or state ' +
+                    'name and I will provide you with updated Corona stats.\nFor example: \npb ' +
+                    'for Punjab\njk for Jammu and Kashmir etc')
+                return
             }
-            let statedata = statewise[stateindex];
-            let data = statedata['state']
+            let statedata = statewise[stateindex]
+            let data = '<b>' + statedata['state'] + '</b>'
             data += "\nConfirmed Cases: " + statedata['confirmed']
             data += "\nActive Cases: " + statedata['active']
             data += "\nRecovered: " + statedata['recovered']
             data += "\nDeaths: " + statedata['deaths']
 
             // send a message to the chat
-            bot.sendMessage(chatId, data);
-            return;
-        };
-    });
-
-});
+            bot.sendMessage(chatId, data, {
+                parse_mode: 'HTML'
+            })
+            return
+        }
+    })
+})
