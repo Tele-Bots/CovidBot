4b7d29523f58ba70c61cb5945d5a93e4dec169a9
Rahul Gurung <gurungrahul2@gmail.com>
2020-05-12 22:08:09 +0530

    updated the error message and included the commands

--- app/utils/prepareError.js
+++ app/utils/prepareError.js
@@ -1,8 +1,11 @@
 function prepareError(bot, chatId) {
     bot.sendMessage(chatId,
-        'Sorry ! I am not able to understand. Send me your state code or state ' +
-        'name and I will provide you with updated Corona stats.\nFor example: \n<i>pb</i> ' +
-        'for Punjab\n<i>jk</i> for Jammu and Kashmir etc', {
+        `Oops ! I am too dumb to understand that. Just send the statename or statecode.\n` +
+        `Or try one of these:\n` +
+        `/all - all india stats with top 15 states.\n` +
+        `/daily - daily new cases for past 5 days.\n` +
+        `test statename - get Covid19 test labs from the states.\n` +
+        `daily N - daily new cases for past N days.`, {
         parse_mode: 'HTML'
     })
 }
