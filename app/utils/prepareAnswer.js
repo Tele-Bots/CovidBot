54948fde050b05fdd41d2628b34b4ace5359e1bb
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-06-08 22:39:37 +0530

    Removed the unnecessary else part

--- app/utils/prepareAnswer.js
+++ app/utils/prepareAnswer.js
@@ -18,17 +18,16 @@ function prepareStatsCompactAnswer(body, index, nameState) {
         data += '<i> (+' + numberWithIndianCommas(statedata['deltaconfirmed']) + ')</i>'
     }
     data += "\n\u{1F7E0} Active: " + numberWithIndianCommas(statedata['active'])
-    if (parseInt(statedata['deltarecovered']) > 0) {
+ 
         let deltaActive = parseInt(statedata['deltaconfirmed']) - parseInt(statedata['deltarecovered']) - parseInt(statedata['deltadeaths'])
+       //change in active number will not be displayed if it is zero.
         if (deltaActive > 0)
             data += '<i> (+' + numberWithIndianCommas(deltaActive) + ')</i>'
         else if (deltaActive < 0) {
+            //required to display change in active numbers correctly if active number decreases
             deltaActive = deltaActive + (-2) * deltaActive
             data += '<i> (-' + numberWithIndianCommas(deltaActive) + ')</i>'
         }
-        else
-            data = data
-    }
     data += "\n\u{1F7E2} Recovered: " + numberWithIndianCommas(statedata['recovered'])
     if (parseInt(statedata['deltarecovered']) > 0) {
         data += '<i> (+' + numberWithIndianCommas(statedata['deltarecovered']) + ')</i>'
