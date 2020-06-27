cc08443493d8f8b95a7b6b7b148fc1ef7907a2ca
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-06-27 15:14:10 +0530

    Updated code as suggested

--- app/utils/prepareAnswer.js
+++ app/utils/prepareAnswer.js
@@ -126,28 +126,23 @@ function prepareDailyStatsAnswer(body, n) {
 
 function prepareTestingResourceAnswer(stateResources) {
   if (stateResources.length === 0) {
-    const temp = ['\n\n\u{1F9EA} No COVID-19 Testing Labs found', ''];
+    const temp = ['\n\n\u{1F9EA} No COVID-19 Testing Labs found'];
     return temp;
   }
 
-  let data = `\n\n\u{1F9EA} COVID-19 Testing Labs in ${stateResources[0].state}, found ${stateResources.length} results\n`;
-  let data2 = '';
+  const data = [`\n\n\u{1F9EA} COVID-19 Testing Labs in ${stateResources[0].state}, found ${stateResources.length} results\n`];
+  data[0] += `\n<b>${stateResources[0].nameoftheorganisation}, ${stateResources[0].city}\n`
+  + `</b>Description: ${stateResources[0].descriptionandorserviceprovided}.\n`;
+  if (stateResources[0].phonenumber.length > 0) { data[0] += `<i>Phone Number: ${stateResources[0].phonenumber}</i>\n`; }
+  if (stateResources[0].contact.length > 0) { data[0] += `<i>Website: ${stateResources[0].contact}</i>\n`; }
 
-  for (let i = 0; i < stateResources.length; i += 1) {
-    if (i <= 19) {
-      data += `\n<b>${stateResources[i].nameoftheorganisation}, ${stateResources[i].city}\n`
+  for (let i = 1; i < stateResources.length; i += 1) {
+    data[i] = `\n<b>${stateResources[i].nameoftheorganisation}, ${stateResources[i].city}\n`
         + `</b>Description: ${stateResources[i].descriptionandorserviceprovided}.\n`;
-      if (stateResources[i].phonenumber.length > 0) { data += `<i>Phone Number: ${stateResources[i].phonenumber}</i>\n`; }
-      if (stateResources[i].contact.length > 0) { data += `<i>Website: ${stateResources[i].contact}</i>\n`; }
-    } else {
-      data2 += `\n<b>${stateResources[i].nameoftheorganisation}, ${stateResources[i].city}\n`
-        + `</b>Description: ${stateResources[i].descriptionandorserviceprovided}.\n`;
-      if (stateResources[i].phonenumber.length > 0) { data2 += `<i>Phone Number: ${stateResources[i].phonenumber}</i>\n`; }
-      if (stateResources[i].contact.length > 0) { data2 += `<i>Website: ${stateResources[i].contact}</i>\n`; }
-    }
+    if (stateResources[i].phonenumber.length > 0) { data[i] += `<i>Phone Number: ${stateResources[i].phonenumber}</i>\n`; }
+    if (stateResources[i].contact.length > 0) { data[i] += `<i>Website: ${stateResources[i].contact}</i>\n`; }
   }
-  const passData = [data, data2];
-  return passData;
+  return data;
 }
 
 function prepareNewTopStatesStat(body) {
