119acd981cd47adb0b74416e188a336c7b249c3b
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-07-18 18:53:12 +0530

    Changed daily default from 5 to 10

--- app/commands/daily.js
+++ app/commands/daily.js
@@ -1,6 +1,6 @@
 const prepareAnswer = require('../utils/prepareAnswer');
 
-function daily(body, bot, chatId, n = 5) {
+function daily(body, bot, chatId, n = 10) {
   const data = prepareAnswer.prepareDailyStatsAnswer(body, n);
   return bot.sendMessage(chatId, data, {
     parse_mode: 'HTML',
