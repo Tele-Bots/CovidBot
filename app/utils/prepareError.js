7b7d384e76ef70bc1fd4609181630c71e32a9896
Rahul Gurung <gurungrahul2@gmail.com>
2020-08-13 23:46:40 +0530

    improved error message

--- app/utils/prepareError.js
+++ app/utils/prepareError.js
@@ -3,9 +3,11 @@ function prepareError(bot, chatId) {
     'Oops ! I am too dumb to understand that. Just send the statename or statecode.\n'
     + 'Or try one of these:\n'
     + '/all - all india stats with top 15 states.\n'
-    + '/daily - daily new cases for past 5 days.\n'
-    + 'test statename - get Covid19 test labs from the states.\n'
-    + 'daily N - daily new cases for past N days.', {
+    + '/daily - daily new cases for past 10 days.\n'
+    + '/daily_active - daily new active cases for past 10 days.\n'
+    + '/new - all the states with sorted daily new cases.\n'
+    + '/test_pb - get Covid19 test labs from punjab (or any other state).\n'
+    + '/daily_30 - daily new cases for past 30 days.', {
       parse_mode: 'HTML',
     });
 }
