42680c133dd44d656175a7cec69d2cb915c0873f
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-09-04 01:28:37 +0530

    added short url for `graph` command

--- app/commands/graph.js
+++ app/commands/graph.js
@@ -1,9 +1,9 @@
 const prepareAnswer = require('../utils/prepareAnswer');
 
 function graph(body, bot, chatId, n = 10) {
-  const data = prepareAnswer.summaryGraph(body, n);
+  prepareAnswer.summaryGraph(body, n, chatId, bot);
 
-  return bot.sendPhoto(chatId, data);
+  return true;
 }
 
 module.exports = {
